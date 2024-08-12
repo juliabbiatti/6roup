4c80ca2373eedc364e624e5f6386e91e283603dd
Julieta <julietaabbiatti6@gmail.com>
2024-08-12 13:51:40 -0300

    api

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/6roup/',  // Asegúrate de que este sea el nombre correcto de tu repositorio en GitHub
+  base: '/6roup/',  
   build: {
-    outDir: 'dist'  // Esto es opcional si 'dist' ya es el valor por defecto, pero puedes dejarlo para mayor claridad
+    outDir: 'dist'  
   }
 })
